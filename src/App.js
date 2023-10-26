b8fb0582cf536e5d73343e70344c1a712d8a7ff8
Amal911 <amaleattath911@gmail.com>
2023-10-26 21:29:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+//46fbf0de
+
+const API_URL = "http://www.omdbapi.com/?apikey=46fbf0de&";
+const mov = [
+  {
+    Title: "Batman Begins",
+    Year: "2005",
+    imdbID: "tt0372784",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  },
+  {
+    Title: "Batman v Superman: Dawn of Justice",
+    Year: "2016",
+    imdbID: "tt2975590",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Batman",
+    Year: "2022",
+    imdbID: "tt1877830",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BM2MyNTAwZGEtNTAxNC00ODVjLTgzZjUtYmU0YjAzNmQyZDEwXkEyXkFqcGdeQXVyNDc2NTg3NzA@._V1_SX300.jpg",
+  },
+  {
+    Title: "Batman",
+    Year: "1989",
+    imdbID: "tt0096895",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg",
+  },
+  {
+    Title: "Batman Returns",
+    Year: "1992",
+    imdbID: "tt0103776",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOGZmYzVkMmItM2NiOS00MDI3LWI4ZWQtMTg0YWZkODRkMmViXkEyXkFqcGdeQXVyODY0NzcxNw@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Batman & Robin",
+    Year: "1997",
+    imdbID: "tt0118688",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMGQ5YTM1NmMtYmIxYy00N2VmLWJhZTYtN2EwYTY3MWFhOTczXkEyXkFqcGdeQXVyNTA2NTI0MTY@._V1_SX300.jpg",
+  },
+  {
+    Title: "Batman Forever",
+    Year: "1995",
+    imdbID: "tt0112462",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNDdjYmFiYWEtYzBhZS00YTZkLWFlODgtY2I5MDE0NzZmMDljXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lego Batman Movie",
+    Year: "2017",
+    imdbID: "tt4116284",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTcyNTEyOTY0M15BMl5BanBnXkFtZTgwOTAyNzU3MDI@._V1_SX300.jpg",
+  },
+  {
+    Title: "Batman: The Animated Series",
+    Year: "1992–1995",
+    imdbID: "tt0103359",
+    Type: "series",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTM3MTRkZjQtYjBkMy00YWE1LTkxOTQtNDQyNGY0YjYzNzAzXkEyXkFqcGdeQXVyOTgwMzk1MTA@._V1_SX300.jpg",
+  },
+  {
+    Title: "Batman v Superman: Dawn of Justice (Ultimate Edition)",
+    Year: "2016",
+    imdbID: "tt18689424",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTRlNWQwM2ItNjkyZC00MGI3LThkYjktZmE5N2FlMzcyNTIyXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg",
+  },
+];
+const movie1 = {
+  Title: "The Amazing Spiderman 2 Webb Cut",
+  Year: "2021",
+  imdbID: "tt18351128",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg",
+};
 
 function App() {
+  const [movies, setmovies] = useState([]);
+
+  const searchMovies = async (title) => {
+   
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+    setmovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("marvel");
+  }, []);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>CineVerse </h1>
+
+      <div className="search">
+        <input
+          placeholder="Search For Movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => {
+            return <MovieCard movie={movie} />;
+          })}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 }
